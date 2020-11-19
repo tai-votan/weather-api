d7dbbd4c09dd324c6d14f3d0110eb6e4fd903d96
votantai <tai.vo@epsilo.io>
2020-11-19 23:46:03 +0700

    change author

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 6600;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   const { email, password } = req.query;
